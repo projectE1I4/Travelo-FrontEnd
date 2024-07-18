df7fe6bed61a2c105b9eac6bc904e508627ade76
khb13 <120230839+khb13@users.noreply.github.com>
2024-07-18 15:53:15 +0900

    🔧 [Add] 테일윈드 기본 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Pretendard', 'sans-serif'],
+    },
+    extend: {
+      borderRadius: {
+        cus: '10px',
+      },
+      fontSize: {
+        'h1-d': '40px',
+        'h2-d': '32px',
+        'h3-d': '24px',
+        'h1-m': '32px',
+        'h2-m': '24px',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'current',
+        pc1: '#4d76b3',
+        pc2: '#f6f6f6',
+        ac: '#64bdab',
+        txt400: '#cccccc',
+        txt500: '#666666',
+        txt600: '#222222',
+        btntxt: '#ffffff',
+        btnbg: '#ffffff',
+        iconred: '#ff4040',
+      },
+    },
   },
   plugins: [],
 };

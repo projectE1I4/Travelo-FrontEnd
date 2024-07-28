bce20ef8b3dd6838fdcb23a461a3ee562962215e
hyerin <mjhl0717@gmail.com>
2024-07-28 22:54:03 +0900

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from '../components/home/Home';
+import Home from '../components/Home/Home';
 import { useAuth } from '../hooks/useAuth';
 
 const HomePage = () => {

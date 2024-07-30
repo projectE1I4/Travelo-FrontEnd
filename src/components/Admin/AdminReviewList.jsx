5aacdf33497d61a8541c5f628198ea5d533de3dc
hyerin <mjhl0717@gmail.com>
2024-07-30 17:38:36 +0900

    💄 관리자 css 완료

--- src/components/Admin/AdminReviewList.jsx
+++ src/components/Admin/AdminReviewList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   getAllReviews,
   deleteReview,
@@ -6,21 +6,25 @@ import {
 } from '../../services/adminService';
 import { formatDate } from '../common/formatDate';
 import '../../css/Admin/AdminReview.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const AdminReviewList = () => {
   const [reviews, setReviews] = useState([]);
   const [selectedReviews, setSelectedReviews] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [sortBy, setSortBy] = useState('latest'); // 기본 정렬값 설정
-  const [filter, setFilter] = useState('all'); // 필터링 조건
-  const [page, setPage] = useState(0); // 페이지 상태 추가
+  const [sortBy, setSortBy] = useState('latest');
+  const [filter, setFilter] = useState('all');
+  const [page, setPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('최신순');
+  const dropdownRef = useRef(null);
 
   const fetchReviews = async (page, sortOrder) => {
     try {
       const data = await getAllReviews(page, sortOrder);
-      console.log('Fetched reviews:', data);
       setReviews(data);
       setLoading(false);
     } catch (error) {
@@ -33,11 +37,33 @@ const AdminReviewList = () => {
     fetchReviews(page, sortBy);
   }, [page, sortBy]);
 
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
+  const handleFilterChange = (filter) => {
+    setFilter(filter);
   };
 
-  // 필터링된 리뷰 목록
+  const handleDropdownClick = (option) => {
+    setSelectedOption(option);
+    setSortBy(option === '최신순' ? 'latest' : 'oldest');
+    setDropdownOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   const filteredReviews = reviews.filter((item) => {
     if (filter === 'all') return true;
     return filter === 'blind'
@@ -45,7 +71,6 @@ const AdminReviewList = () => {
       : item.review.blindYn === 'N';
   });
 
-  // 전체 리뷰, 블라인드된 리뷰, 보이는 리뷰 개수 계산
   const allCount = reviews.length;
   const blindCount = reviews.filter(
     (item) => item.review.blindYn === 'Y'
@@ -69,7 +94,6 @@ const AdminReviewList = () => {
     }
   };
 
-  //선택 리뷰 삭제
   const deleteSelectedReviews = async () => {
     if (selectedReviews.length === 0) {
       alert('삭제할 리뷰를 선택하세요.');
@@ -104,44 +128,96 @@ const AdminReviewList = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="adminReviewList">
+    <div className="adminReviewList adminUserList">
       <h1>전체 리뷰</h1>
-      <div className="controls">
-        <div>
-          <button onClick={() => setSortBy('latest')}>최신순</button>
-          <button onClick={() => setSortBy('oldest')}>오래된 순</button>
+      <div className="filtering">
+        <div className="filter">
+          <button
+            onClick={() => handleFilterChange('all')}
+            className={filter === 'all' ? 'active' : ''}
+          >
+            전체 ({allCount})
+          </button>
+          <button
+            onClick={() => handleFilterChange('blind')}
+            className={filter === 'blind' ? 'active' : ''}
+          >
+            블라인드된 리뷰 ({blindCount})
+          </button>
+          <button
+            onClick={() => handleFilterChange('visible')}
+            className={filter === 'visible' ? 'active' : ''}
+          >
+            보이는 리뷰 ({visibleCount})
+          </button>
         </div>
-        <button onClick={deleteSelectedReviews}>선택한 리뷰 삭제</button>
-        <select onChange={handleFilterChange} value={filter}>
-          <option value="all">전체 ({allCount})</option>
-          <option value="blind">블라인드된 리뷰 ({blindCount})</option>
-          <option value="visible">보이는 리뷰 ({visibleCount})</option>
-        </select>
-      </div>
-      <ul>
-        {filteredReviews.map((item, index) => (
-          <li key={`${item.review.reviewSeq}-${index}`}>
-            <input
-              type="checkbox"
-              onChange={() => toggleSelectReview(item.review.reviewSeq)}
-              checked={selectedReviews.includes(item.review.reviewSeq)}
+        <div className="sort" ref={dropdownRef}>
+          <button onClick={toggleDropdown} className="dropDownBtn">
+            <div className="text">{selectedOption}</div>
+            <FontAwesomeIcon
+              icon={dropdownOpen ? faChevronUp : faChevronDown}
             />
-            코스 제목: {item.courseTitle}
-            <br />
-            작성자: {item.review.user?.username || 'Unknown'}, 작성 일자:{' '}
-            {item.review.createDate
-              ? formatDate(item.review.createDate)
-              : '알 수 없음'}
-            <br />
-            <span className="reviewContent">댓글: {item.review.content}</span>
-            <br />
-            신고 수: {item.review.reportCount}
-            <button onClick={() => reviewDeleteBtn(item.review.reviewSeq)}>
-              삭제
-            </button>
-          </li>
-        ))}
-      </ul>
+          </button>
+          {dropdownOpen && (
+            <div className={`dropdownContent ${dropdownOpen ? 'show' : ''}`}>
+              <a onClick={() => handleDropdownClick('최신순')}>최신순</a>
+              <a onClick={() => handleDropdownClick('오래된순')}>오래된순</a>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="userSelect">
+        <button onClick={deleteSelectedReviews}>선택한 리뷰 삭제</button>
+        <p>선택된 리뷰: {selectedReviews.length}</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th style={{ width: '8%' }}>선택</th>
+            <th style={{ width: '8%' }}>리뷰 번호</th>
+            <th style={{ width: '15%' }}>코스 제목</th>
+            <th style={{ width: '15%' }}>작성자</th>
+            <th style={{ width: '15%' }}>댓글</th>
+            <th style={{ width: '12%' }}>작성 일자</th>
+            <th style={{ width: '8%' }}>신고 수</th>
+            <th style={{ width: '10%' }}>삭제</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredReviews.map((item, index) => (
+            <tr key={`${item.review.reviewSeq}-${index}`}>
+              <td>
+                <input
+                  type="checkbox"
+                  className="styled-checkbox"
+                  id={`checkbox-${item.review.reviewSeq}`}
+                  onChange={() => toggleSelectReview(item.review.reviewSeq)}
+                  checked={selectedReviews.includes(item.review.reviewSeq)}
+                />
+                <label htmlFor={`checkbox-${item.review.reviewSeq}`}></label>
+              </td>
+              <td>{item.review.reviewSeq}</td>
+              <td>{item.courseTitle}</td>
+              <td>{item.review.user?.username || 'Unknown'}</td>
+              <td className="reviewContent">{item.review.content}</td>
+              <td>{item.review.reportCount}</td>
+              <td>
+                {item.review.createDate
+                  ? formatDate(item.review.createDate)
+                  : '알 수 없음'}
+              </td>
+              <td>
+                <button
+                  className="delBtn btn_type_1"
+                  onClick={() => reviewDeleteBtn(item.review.reviewSeq)}
+                >
+                  삭제
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <div className="pagination">
         <button onClick={() => setPage(page - 1)} disabled={page <= 0}>
           이전

3221921050d7903215863f93ea031f22c9d2f1bf
박진희 <melbazzang@naver.com>
2024-07-30 12:08:22 +0900

    💄 [Sytle] 코스 커스텀 스타일링 완료

--- src/components/courseCustom/CourseMiniCard.jsx
+++ src/components/courseCustom/CourseMiniCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../styles/components/courseCustom/CourseMiniCard.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faPlus, faImage } from '@fortawesome/free-solid-svg-icons';
 import { useCourse } from '../../contexts/CourseContext';
 
 const CourseMiniCard = ({ place }) => {
@@ -34,15 +34,19 @@ const CourseMiniCard = ({ place }) => {
 
   return (
     <div className={styles.card} onClick={handleCardClick}>
-      {place.imageFile1 ? (
-        <img
-          src={place.imageFile1}
-          alt={place.title}
-          className={styles.image}
-        />
-      ) : (
-        <div className={styles.noImage}>이미지 없음</div>
-      )}
+      <div className={styles['image-container']}>
+        {place.imageFile1 ? (
+          <img
+            src={place.imageFile1}
+            alt={place.title}
+            className={styles.image}
+          />
+        ) : (
+          <div className={styles['image-placeholder']}>
+            <FontAwesomeIcon icon={faImage} size="2x" color="#ccc" />
+          </div>
+        )}
+      </div>
       <div className={styles.details}>
         <h4 className={styles.name}>{place.title}</h4>
         <p className={styles.address}>{place.address}</p>
@@ -50,11 +54,11 @@ const CourseMiniCard = ({ place }) => {
           <span className={styles.likes}>
             <FontAwesomeIcon icon={faHeart} /> {place.likeCount}
           </span>
-          <button className={styles.addButton} onClick={handleAddClick}>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
         </div>
       </div>
+      <button className={styles.addButton} onClick={handleAddClick}>
+        <FontAwesomeIcon icon={faPlus} />
+      </button>
     </div>
   );
 };
--- src/components/courseCustom/CourseSaveModal.jsx
+++ src/components/courseCustom/CourseSaveModal.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useCourse } from '../../contexts/CourseContext';
 import axiosInstance from '../../utils/axiosInstance';
 import styles from '../../styles/components/courseCustom/CourseSaveModal.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
 const CourseSaveModal = ({ onClose }) => {
   const { selectedPlaces, selectedRegion } = useCourse();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [privateYn, setPrivateYn] = useState('N'); // 기본값을 비공개로 설정
+  const [privateYn, setPrivateYn] = useState('Y'); // 기본값을 비공개로 설정
 
   useEffect(() => {
     // 모달이 열릴 때 body에 overflow: hidden; 스타일을 추가
@@ -40,6 +38,7 @@ const CourseSaveModal = ({ onClose }) => {
         },
       });
       console.log('코스 저장 성공:', response.data);
+      alert('코스 저장이 완료되었습니다');
       onClose();
     } catch (error) {
       console.error('코스 저장 실패:', error);
@@ -51,37 +50,48 @@ const CourseSaveModal = ({ onClose }) => {
   };
 
   return (
-    <div className={styles['modal']}>
-      <div className={styles['modalContent']}>
-        <h2>코스 저장</h2>
-        <label>
-          제목:
+    <div className={styles.modal}>
+      <div className={styles['modal-content']}>
+        <h2>코스 커스텀 저장</h2>
+        <div className={styles['form-group']}>
+          <label className={styles.label}>코스제목</label>
           <input
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
+            placeholder="코스 제목을 입력해주세요"
+            className={styles.input}
           />
-        </label>
-        <label>
-          설명:
+        </div>
+        <div className={styles['form-group']}>
+          <label className={styles.label}>설명(선택사항)</label>
           <textarea
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-          />
-        </label>
-        <div className={styles['toggle-container']}>
-          <span className={styles['toggle-label']}>
-            공개 여부: {privateYn === 'N' ? '비공개' : '공개'}
-          </span>
-          <FontAwesomeIcon
-            icon={privateYn === 'N' ? faToggleOff : faToggleOn}
-            onClick={togglePrivacy}
-            className={styles['toggle-icon']}
-            size="2x"
+            placeholder="코스 설명을 입력해주세요"
+            className={styles.textarea}
           />
         </div>
-        <button onClick={handleSave}>저장</button>
-        <button onClick={onClose}>취소</button>
+
+        <div className={styles['toggle-container']}>
+          <span className={styles['toggle-label']}>코스 공개하기</span>
+          <label className={styles.switch}>
+            <input
+              type="checkbox"
+              checked={privateYn === 'N'}
+              onChange={togglePrivacy}
+            />
+            <span className={styles.slider}></span>
+          </label>
+        </div>
+        <div className={styles['btn-wrap']}>
+          <button onClick={handleSave} className={styles['save-button']}>
+            저장하기
+          </button>
+          <button onClick={onClose} className={styles['cancel-button']}>
+            취소
+          </button>
+        </div>
       </div>
     </div>
   );

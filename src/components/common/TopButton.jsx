e77fca375c6d63cce8fbb4b2a01f016f2ef04900
khb13 <kij5480@gmail.com>
2024-07-31 06:50:58 +0900

    🎈[Fix] 탑버튼 리사이징 시 대응

--- src/components/common/TopButton.jsx
+++ src/components/common/TopButton.jsx
@@ -3,6 +3,7 @@ import styles from '../../styles/components/TopButton.module.css'; // CSS 파일
 
 const TopButton = () => {
   const [showButton, setShowButton] = useState(false);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight); // 창의 높이를 상태로 관리
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -12,19 +13,23 @@ const TopButton = () => {
   };
 
   useEffect(() => {
-    const ShowButtonClick = () => {
-      const halfWindowHeight = window.innerHeight / 2; // 창 높이의 절반을 계산합니다.
-      if (window.scrollY > halfWindowHeight) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
+    const handleScroll = () => {
+      const threshold = windowHeight * 0.5; // 높이의 50%를 기준으로 설정
+      setShowButton(window.scrollY > threshold);
     };
-    window.addEventListener('scroll', ShowButtonClick);
+
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight); // 창 크기가 변경되면 창 높이를 업데이트
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize); // resize 이벤트 리스너 추가
+
     return () => {
-      window.removeEventListener('scroll', ShowButtonClick);
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize); // cleanup 시 resize 이벤트 리스너 제거
     };
-  }, []);
+  }, [windowHeight]);
 
   return (
     <>
